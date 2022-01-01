c097f2e4939659b5f1b46e010427739051786bd7
Abhishek Vithal <78862562+99003612@users.noreply.github.com>
2022-01-01 23:57:00 +0530

    Linked lists questions

---
+++ Recursion/Linked List/PepCoding/OneIntroduction.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class OneIntroduction
+{
+    public static class Node {
+        int data;
+        Node next;
+    }
+
+    public static class LinkedList{
+        Node head;
+        Node tail;
+        int size;
+    }
+}
