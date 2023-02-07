13be1df34e1c972de88da3b43cd07743bfe2023f
Abhishek Vithal <40138618+vithalabhishek@users.noreply.github.com>
2023-02-07 23:38:26 +0530

    Time: 89 ms (21.41%), Space: 42.5 MB (65.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> sum = new HashMap<Integer,Integer>();
-        int[] ans = new int[2];
-        
-        for(int i = 0; i < nums.length;i++){
-            if(sum.containsKey(target - nums[i])){
-                ans[1] = i;
-                ans[0] = sum.get(target - nums[i]);
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1 ; j < nums.length;j++){
+                if(nums[j] == target - nums[i]){
+                    return new int[]{i,j};
+                }
             }
-            sum.put(nums[i], i);
         }
-        return ans;
+        throw new IllegalArgumentException("TWO SUM NO");
     }
 }
