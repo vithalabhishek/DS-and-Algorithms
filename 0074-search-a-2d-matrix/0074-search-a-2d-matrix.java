001eee7e3c55a76829163831faf4134880270b80
Abhishek Vithal <40138618+vithalabhishek@users.noreply.github.com>
2023-02-28 00:08:29 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (77.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,20 +1,20 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        //Optimal Solution for Leetcode
         if(matrix.length == 0) return false;
         
-        int n = matrix.length, m = matrix[0].length;
+        int n = matrix.length;
+        int m = matrix[0].length;
         
-        int low = 0, high = (n * m) - 1;
-        //Applying binary search
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(matrix[mid/m][mid % m] == target) //mid/m gives row and mid % m give column
-                return true;
-            else if(matrix[mid/m][mid % m] > target)
-                high = mid - 1;
-            else
-                low = mid + 1;
+        int lo = 0;
+        int hi = (n*m) - 1;
+        
+        while(lo <= hi){
+            int mid = (lo + (hi))/2;
+            if(matrix[mid/m][mid % m] == target){ return true;}
+            if(matrix[mid/m][mid%m] < target) lo = mid + 1;
+            else{
+                hi = mid - 1;
+            }
         }
         return false;
     }
